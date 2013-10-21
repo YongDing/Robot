6a8f12b5211a7a0cd0acf72e541527c1b0f7ef70
YongDing <dytack0815@gmail.com>
2013-10-22 01:18:58 +0800

    add library

--- src/sample/Robotd.java
+++ src/sample/Robotd.java
@@ -27,13 +27,13 @@ public class Robotd extends AdvancedRobot {
 	
     public String readToBuffer(StringBuffer buffer, String filePath) throws IOException {
         InputStream is = new FileInputStream(filePath);
-        String line; // 用来保存每行读取的内容
+        String line;
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        line = reader.readLine(); // 读取第一行
-        while (line != null) { // 如果 line 为空说明读完了
-            buffer.append(line); // 将读到的内容添加到 buffer 中
-            buffer.append("\n"); // 添加换行符
-            line = reader.readLine(); // 读取下一行
+        line = reader.readLine();
+        while (line != null) {
+            buffer.append(line); 
+            buffer.append("\n"); 
+            line = reader.readLine(); 
         }
         reader.close();
         is.close();
